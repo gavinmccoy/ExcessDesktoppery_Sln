0bcf3cceb59909bbc7953649d0337888d33ee36e
gavinmccoy <gavin.mccoy2@mail.dcu.ie>
2016-09-14 23:49:03 +0100

    First commit.

---
+++ ExcessDesktoppery/Excess.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcessDesktoppery
+{
+    class Excess
+    {
+        public static void MoveUnusedFiles(string sourcePath, string targetPath)
+        {
+            List<string> files = Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories).ToList();
+
+            if (!System.IO.Directory.Exists(targetPath))
+            {
+                System.IO.Directory.CreateDirectory(targetPath);
+            }
+
+            foreach (string item in files)
+            {
+                try
+                {
+                    FileInfo file = new FileInfo(item);
+
+                    if (file.LastAccessTime < DateTime.Now.AddDays(-30))
+                    {
+                        if (!(new FileInfo(targetPath + "\\" + file.Name).Exists))
+                        {
+                            file.MoveTo(targetPath + "\\" + file.Name);
+                        }
+                    }
+                }
+                catch (PathTooLongException)
+                {
+
+                    //throw;
+                }
+                
+
+                
+
+
+            }
+        }
+    }
+}
--- ExcessDesktoppery/Program.cs
+++ ExcessDesktoppery/Program.cs
@@ -10,6 +10,12 @@ namespace ExcessDesktoppery
     {
         static void Main(string[] args)
         {
+            // Moves files on the desktop that have not been accessed for 30 days to
+            // a folder called Excess Desktoppery
+            // Any improvements would be greatly appreciated!!!
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string newFolder = path + "\\Excess Desktoppery".ToString();
+            Excess.MoveUnusedFiles(path, newFolder);
         }
     }
 }
